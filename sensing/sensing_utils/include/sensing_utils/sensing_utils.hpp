daec64778f3b207babc4c940bf552108589a1371
scepter914 <scepter914@gmail.com>
2022-10-02 22:20:41 +0900

    add template
    Signed-off-by: scepter914 <scepter914@gmail.com>

---
+++ sensing/sensing_utils/include/sensing_utils/sensing_utils.hpp
@@ -0,0 +1,57 @@
+// Copyright 2022 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SENSING_UTILS__SENSING_UTILS_HPP__
+#define SENSING_UTILS__SENSING_UTILS_HPP__
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "rclcpp/logger.hpp"
+
+namespace sensing_utils
+{
+class SensingUtils
+{
+public:
+  explicit SensingUtils(const rclcpp::Logger & logger) : logger_(logger) {}
+
+  struct Input
+  {
+    int data{};
+  };
+
+  struct Output
+  {
+    int data{};
+  };
+
+  struct Param
+  {
+    int data{};
+  };
+
+  void setParam(const Param & param) { param_ = param; }
+
+  Output update(const Input & input);
+
+private:
+  rclcpp::Logger logger_;
+  Param param_{};
+};
+
+}  // namespace sensing_utils
+
+#endif  // SENSING_UTILS__SENSING_UTILS_HPP__
